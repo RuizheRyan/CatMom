4b3ce2f62a9828bc20075590e5e3d66f35f1d761
JYLYJ <45932906+JYLYJ@users.noreply.github.com>
2020-04-23 20:01:12 -0700

    Door_Test

---
+++ The Last of Cats/Assets/Script/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] GameObject key;
+    [SerializeField] GameObject door;
+
+    static readonly Vector3[] keyPosition = {
+        new Vector3(0, 0.12f, 0),
+        new Vector3(0, 0.06f, 0),
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = transform.Find("Key").gameObject;
+        key.transform.localPosition = keyPosition[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        key.transform.localPosition = keyPosition[1];
+
+        // Open the door
+        door.GetComponent<DoorController>().isOpen = true;
+    }
+}
---
+++ The Last of Cats/Assets/Script/DoorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public bool isOpen = false;
+    float rotation = 0.0f;
+    const float rotateSpeed = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        rotation = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen && rotation <= 87.0f)
+        {
+            transform.Rotate(new Vector3(0, - rotateSpeed * Time.deltaTime, 0));
+            rotation += rotateSpeed * Time.deltaTime;
+        }
+    }
+}
