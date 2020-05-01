ce8230e7315334a2d02af7a451e72032e2b1b4dc
Ryan Tung <53037140+13126789556@users.noreply.github.com>
2020-05-01 04:11:34 -0700

    final version

---
+++ The Last of Cats/Assets/Script/FearZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FearZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("kitten"))
+        {
+            other.GetComponent<AIController>().fear = 1;
+        }
+    }
+}
