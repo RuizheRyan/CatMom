472c2d3cb51cb1f668bcf720cd7ac02eed9ccb62
JYLYJ <45932906+JYLYJ@users.noreply.github.com>
2020-05-01 03:29:12 -0700

    end

---
+++ The Last of Cats/Assets/Script/GameOver.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    const int numMax = 3;
+    [SerializeField] HashSet<GameObject> cat = new HashSet<GameObject>();
+    float fear = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "kitten")
+        {
+            cat.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "kitten")
+        {
+            cat.Remove(other.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        bool isFear = false;
+        foreach(GameObject c in cat) 
+        {
+            if (c.tag == "kitten" && c.GetComponent<AIController>().fear > 0)
+            {
+                isFear = true;
+            }
+        }
+
+        if (cat.Count == numMax && !isFear)
+        {
+            SceneManager.LoadScene("forest", LoadSceneMode.Single);
+        }
+    }
+}
